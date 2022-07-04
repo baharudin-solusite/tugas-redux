ddf37a885aea585294b16bd00d6dd43597644d12
Baharudin <baharudinfahrul85@gmail.com>
2022-07-05 06:23:23 +0700

    Buat sebuah react-redux project

---
+++ src/component/CakeView.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { decrementCake, restockCake } from "./CakeSlice";
+
+const CakeView = () => {
+  const totalCakesRedux = useSelector((state) => state.cake.totalCakes);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>CakeView</div>
+      <h1>Total Cakes: {totalCakesRedux}</h1>
+      <button onClick={() => dispatch(decrementCake())}>Decrement Cake</button>
+      <button onClick={() => dispatch(restockCake())}>Restock Cake</button>
+    </div>
+  );
+};
+
+export default CakeView;
